caa8a105e149c0420cbac8dd071fcc562fc367a0
tovy14 <virgilt2005@yahoo.com>
2024-12-03 15:38:35 +0200

    word_counter

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ def read_content(path):
         return file.read()
 
 
+def word_counter(input_string):
+    counter = 0
+    words = input_string.split()
+    for word in words:
+        counter += 1
+    return counter
+
+
 def main():
     book_path = "books/frankenstein.txt"
-    print(read_content(book_path))
+    book_string = read_content(book_path)
+    print(word_counter(book_string))
 
 
 main()
